c44ffa0c03f12b4d5ac0879b7932283451596ce6
midhunsedge <midhuns858902@gmail.com>
2024-05-03 20:27:09 +0530

    changed

--- Aathmajyothi/settings.py
+++ Aathmajyothi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6t-*u_ged5ubt2in9-$7xdpv#7whlv&i9ymqy#wtk_p*ldlkty
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['www.aathmajyothi.com','aathmajyothi.com','127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
